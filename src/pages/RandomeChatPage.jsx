5365499911f36edfe83e0360efd0aec54772bd33
Heeneth sai <114326514+Heeneth12@users.noreply.github.com>
2023-12-28 15:46:12 +0530

    Create RandomeChatPage.jsx
    added randomechat page

---
+++ src/pages/RandomeChatPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+import RandomeChat from "../components/RandomeChat";
+
+function RandomeChatPage() {
+  return (
+    <>
+      <NavBar />
+      <RandomeChat />
+    </>
+  );
+}
+
+export default RandomeChatPage;
